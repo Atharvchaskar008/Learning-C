72dae26c951e2f2e550475a8c39c6c4b86c6b28f
Atharv Chaskar <atharvchaskar0001@gmail.com>
2026-03-25 11:04:11 +0530

    learned first commited last

---
+++ Hello World/hello.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+# include<conio.h>
+
+int main(){
+    float radius;
+    printf("enter radius");
+    scanf("%f",&radius);
+    printf("Area of radius is%f",3.14*radius*radius);
+    return 0;
+};
