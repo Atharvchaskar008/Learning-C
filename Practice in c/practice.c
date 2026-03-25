115de07fef8298b6904b90fc4d3584bac55cbcbf
Atharv Chaskar <atharvchaskar0001@gmail.com>
2026-03-25 11:02:48 +0530

    practice of c language

---
+++ Practice in c/practice.c
@@ -0,0 +1,13 @@
+//program to calculate perimeter of rectangle
+
+#include <stdio.h>
+int main(){
+   float a,b;
+   printf("enter the length of rectangle");
+   scanf("%f",&a);
+   printf("enter the width of rectangle");
+   scanf("%f",&b);
+   printf("Area of rectangle is %f",a*b);
+};
+
+
---
+++ Practice in c/practice2.c
@@ -0,0 +1,12 @@
+//Program to calculate the cube of a number
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number n");
+    scanf("%d",&n);
+
+    printf("Cube of number n is %d",n*n*n);
+    return 0;
+}
+
